7cf24fe7bdc2ae1f3b686c05ae206a7fda8deca4
Michael Giuliani <michael.pm@libero.it>
2018-04-18 16:28:48 +0200

    working search bar

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -2,9 +2,10 @@ class DogsController < ApplicationController
   before_action :set_dog, only: [:show, :edit, :update, :destroy]
 
   def index
+    @text_input_from_user = params["search-word"]
     @dogs = policy_scope(Dog)
-    if params.has_key?(:q)
-      @dogs = Dog.where('location LIKE ?', "%#{params[:q]}%")
+    if params.has_key?("search-word")
+      @dogs = Dog.where('location LIKE ?', "%#{@text_input_from_user.capitalize}%")
     else
       @dogs = Dog.all
     end
