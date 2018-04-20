d255620717b770e8eab5e23af6a3cdbdd9a08705
Luis Carlos Quarta <djlevisn1@gmail.com>
2018-04-20 14:59:59 +0200

    Cards sistemate + mappa

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20180419094753) do
     t.integer "age"
     t.boolean "pedigree"
     t.text "description"
-    t.string "location"
+    t.string "address"
     t.bigint "user_id"
     t.bigint "breed_id"
     t.datetime "created_at", null: false
